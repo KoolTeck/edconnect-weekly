1b38e4f1fbfa29be16d73d51d841a14a4053c683
Adetunji Adebowale <66047485+KoolTeck@users.noreply.github.com>
2021-03-20 21:56:23 +0100

    updating week 6 assgn.

--- app/public/project-explorer/js/app.js
+++ app/public/project-explorer/js/app.js
@@ -213,7 +213,6 @@ const getProjects = async () => {
   if (resp.ok) {
     projects = await resp.json();
     projects.length = 4;
-    projects.reverse();
     projects.forEach((project) => {
       const authors = project.authors.map((tag) => tag.trim()).join(", ");
       const tag = project.tags.map((tag) => tag.trim()).join(" ");
