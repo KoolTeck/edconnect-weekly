3cbea61d786b12483fc748727bef0303c371e6df
Adetunji Adebowale <66047485+KoolTeck@users.noreply.github.com>
2021-05-02 01:19:44 +0100

    updating Week 8 Exercise

--- app/views/Signup.jsx
+++ app/views/Signup.jsx
@@ -8,12 +8,11 @@ const Signup = ({ programs, graduationYears, errors, data }) => {
 
   const formData = data.length > 0 ? JSON.parse(data) : {};
   const [show, setShow] = useState(true);
-  const [firstName, setFirstName] = useState(formData.firstname);
-  const [lastName, setLastName] = useState(formData.lastname);
-  const [matricNumber, setMatricNo] = useState(formData.matricNumber);
-  const [email, setEmail] = useState(formData.email);
-  const [password, setPassword] = useState(formData.password);
-  const refc = useRef(email);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [matricNumber, setMatricNo] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
     <Layout>
@@ -35,18 +34,18 @@ const Signup = ({ programs, graduationYears, errors, data }) => {
         <Form id="signupForm" action="signup" method="post">
           <Form.Row>
             <Col>
-              <Form.Label htmlFor="firstname">First Name</Form.Label>
+              <Form.Label htmlFor="firstName">First Name</Form.Label>
               <Form.Control
                 name="firstName"
-                value={firstName}
+                value={errors.length > 0 ? formData.firstName : firstName}
                 onChange={(eve) => setFirstName(eve.target.value)}
               />
             </Col>
             <Col>
-              <Form.Label htmlFor="lastname">Last Name</Form.Label>
+              <Form.Label htmlFor="lastName">Last Name</Form.Label>
               <Form.Control
                 name="lastName"
-                value={lastName}
+                value={errors.length > 0 ? formData.lastName : lastName}
                 onChange={(eve) => setLastName(eve.target.value)}
               />
             </Col>
@@ -58,9 +57,8 @@ const Signup = ({ programs, graduationYears, errors, data }) => {
               <Form.Control
                 type="email"
                 name="email"
-                value={email}
+                value={errors.length > 0 ? formData.email : email}
                 autoComplete="true"
-                ref={refc}
                 onChange={(eve) => setEmail(eve.target.value)}
               />
             </Col>
@@ -70,7 +68,7 @@ const Signup = ({ programs, graduationYears, errors, data }) => {
                 type="password"
                 autoComplete="true"
                 name="password"
-                value={password}
+                value={errors.length > 0 ? formData.password : password}
                 onChange={(eve) => setPassword(eve.target.value)}
               />
             </Col>
@@ -81,7 +79,7 @@ const Signup = ({ programs, graduationYears, errors, data }) => {
               <Form.Label htmlFor="matricNumber">Matric Number:</Form.Label>
               <Form.Control
                 name="matricNumber"
-                value={matricNumber}
+                value={errors.length > 0 ? formData.matricNumber : matricNumber}
                 onChange={(eve) => setMatricNo(eve.target.value)}
               />
             </Col>
